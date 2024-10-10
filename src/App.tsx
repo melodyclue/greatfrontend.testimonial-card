99762e0c27181b96d5ffc440052cf5ddb3339721
melodyclue <melody.router@gmail.com>
2024-10-10 23:24:02 +0900

    feat: Testimonial Card

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+import { TestimonialCard } from './components/testimonial-card';
+
+export default function App() {
+  return (
+    <div className="flex justify-center">
+      <div className="mt-[200px]">
+        <TestimonialCard />
+      </div>
+    </div>
+  );
+}
